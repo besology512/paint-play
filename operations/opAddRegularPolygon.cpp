02f89c55e51b5f78505adfb0826208c7ceb346c5
Mohamed Elsayed <modimamado161@gmail.com>
2022-11-26 01:24:01 +0200

    Small changes in the status bar messages

--- operations/opAddRegularPolygon.cpp
+++ operations/opAddRegularPolygon.cpp
@@ -22,11 +22,12 @@ void opAddRegularPolygon::Execute()
 	GUI* pUI = pControl->GetUI();
 
 	pUI->PrintMessage("New Regular Polygon: Click at the Center of the Polygon");
-	//Read 1st corner and store in point P1
+
+	//Read the center and store in point center
 	pUI->GetPointClicked(center.x, center.y);
 
 	string msg = "The center is at (" + to_string(center.x) + ", " + to_string(center.y) + " )";
-	msg += " Enter the number of vertices for the regular polygon";
+	msg += " Enter the number of the vertices in the regular polygon";
 	pUI->PrintMessage(msg);
 
 	//Get the number of vertices from the user
@@ -34,27 +35,27 @@ void opAddRegularPolygon::Execute()
 
 	while((int(numOfVertices) - 48 < 3) || (int(numOfVertices) - 48 > 9))
 	{
-		msg = "Error! Please Enter a number of the vertices between 3 and 9";
+		msg = "Error! Make sure that the number of vertices is between 3 and 9";
 		pUI->PrintMessage(msg);
 		pUI->GetKeyClicked(numOfVertices);
 	}
 
 	//Get the radius
-	msg = "Enter the size of the regular polygon";
+	msg = "Enter the side size in the polygonal";
 	pUI->PrintMessage(msg);
 
 	pUI->GetKeyClicked(radius);
 
 	while ((int(radius) - 48 <= 0) || (int(radius) - 48 > 9))
 	{
-		msg = " Error! Please Enter a number of the radius between 1 and 9";
+		msg = " Error! Make Sure that the side size is between 1 and 9";
 		pUI->PrintMessage(msg);
 		pUI->GetKeyClicked(radius);
 	}
 
 	pUI->ClearStatusBar();
 
-	//Preapre all rectangle parameters
+	//Preapre all regular polygon parameters
 	GfxInfo RectGfxInfo;
 
 	//get drawing, filling colors and pen width from the interface
