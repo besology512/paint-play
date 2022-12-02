b9e48846f5feafc4e12907b0d2b77d463137c843
Khaled <s-khaled.mousa@zewailcity.edu.eg>
2022-12-02 12:59:20 +0200

    make save operation to connect controler with Graph and GUI

---
+++ opSave.cpp
@@ -0,0 +1,20 @@
+#include "opSave.h"
+#include "Shapes/Graph.h"
+
+opSave::opSave(controller* pCont): operation(pCont)
+{
+}
+
+opSave::~opSave()
+{
+}
+
+void opSave::Execute()
+{
+
+	//OGraph.Save(outfile);
+	
+	
+
+}
+
---
+++ opSave.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "operations/operation.h"
+#include <fstream>
+
+//Add Save operation class
+class opSave : public operation
+{
+//private :
+	//Graph OGraph; // make compostion of graph to use function in it (object form graph)
+	//ofstream outfile; // make output file to use in Save function 
+public:
+	opSave(controller* pCont);
+	virtual ~opSave();
+
+	//Add Circle to the controller
+	virtual void Execute();
+
+};
