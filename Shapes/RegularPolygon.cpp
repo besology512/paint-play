bdfd8fa7e88cc89603654fa7c15d79b5255eb2b3
Mohamed Elsayed <modimamado161@gmail.com>
2022-12-02 04:13:47 +0200

    The select and unselecet is working with the regular polygon

--- Shapes/RegularPolygon.cpp
+++ Shapes/RegularPolygon.cpp
@@ -18,5 +18,35 @@ void RegularPolygon::Draw(GUI* pUI) const
 bool RegularPolygon::inShape(int x, int y) const
 {
 
-	return false; //Temp
+	//// Creating arrays containing the x,y coordinates of the polygon vertices
+	std::vector<int> xPointsV;
+	std::vector<int> yPointsV;
+
+	const double PI = 3.141592653589;			//Defining constant PI
+	double angle = (2 * PI) / numOfVertices;	//Defining the angle between two vertices
+
+	for (int i = 0; i < int(numOfVertices); i++)
+	{
+		int x = center.x + radius * sin(i * angle);
+		int y = center.y + radius * cos(i * angle);
+
+		xPointsV.push_back(x);
+		yPointsV.push_back(y);
+	}
+
+	int* xPoints = &xPointsV[0];
+	int* yPoints = &yPointsV[0];
+
+
+	int i, j, c = 0;
+	for (i = 0, j = numOfVertices - 1; i < numOfVertices; j = i++) 
+	{
+		if (((yPoints[i] > y) != (yPoints[j] > y)) && (x < (xPoints[j] - xPoints[i]) * (y - yPoints[i]) / (yPoints[j] - yPoints[i]) + xPoints[i]))
+			c = !c;
+	}
+
+	if (c)
+		return true;
+	else
+		return false;
 }
